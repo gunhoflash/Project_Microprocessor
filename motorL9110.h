0e3c848b0911d1b9afbbec9081f99430c6f42190
Heluit <wbjung2917@gmail.com>
2019-12-12 16:44:49 +0900

    edit motorL9110.h

--- motorL9110.h
+++ motorL9110.h
@@ -1,6 +1,9 @@
 #define F_CPU 16000000UL
+
 void motorControl(char c, double quantity)
-{
+{    
+	DDRF=0xf0;
+	DDRA=0xff;
 	int i = 0;
 	int q=quantity*10;
 	switch(c)
@@ -27,23 +30,5 @@ void motorControl(char c, double quantity)
 	for(i=0; i<q; i++)
 	{
 		_delay_ms(1600);
-	}
-}
-int main()
-{
-	DDRF=0xf0;
-	DDRA=0xff;
-	int a=8;
-	char command[4]={'F','B','L','R'};
-	while(1)
-	{
-		//move forward for 1sec
-		motorControl(command[0],0.3);
-		//move backward for 1sec
-		motorControl(command[1],0.3);
-		//turn left for 1sec
-		motorControl(command[2],1.5);
-		//turn right for 3sec
-		motorControl(command[3],3);
 	}
 }
